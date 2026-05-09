01beb2ed61ae9e9f7398292bb615108642251006
MoTong Nie <motongnie@MacBookAir.lan>
2026-05-08 22:36:03 -0700

    Initial backend

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+
+app = FastAPI()
+
+EVENTS = []
+
+
+class Event(BaseModel):
+    user_id: str
+    event_type: str
+    item: str | None = None
+
+
+@app.get("/")
+def home():
+    return {"message": "711 Virtual Pet Backend Running"}
+
+
+@app.post("/event")
+def collect_event(event: Event):
+
+    EVENTS.append({
+        "user_id": event.user_id,
+        "event_type": event.event_type,
+        "item": event.item,
+        "timestamp": str(datetime.now())
+    })
+
+    return {"status": "success"}
+
+
+@app.get("/events")
+def get_events():
+    return EVENTS
